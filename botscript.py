74fea3f1fae505a87be416682edf85c449c20366
Garrett Rector <garrettrector329@gmail.com>
2021-04-03 19:01:33 -0500

    Update botscript.py

--- botscript.py
+++ botscript.py
@@ -18,40 +18,39 @@ def line_find(message):
     return filtered_df.index[0]
 
 
-def record_response_score(message, score):
+def record_response_score(message, score, response):
     df = pd.read_csv("database.csv")
-    datarow = (df[df['parent'] == message].index[0])
-    filtered_df = df[df['parent'].str.contains(message, na=False)]
-    row = filtered_df.index[0]
-    print(datarow)
+    val = df.loc[(df["parent"] == message) & (df["child"] == response), "chance_rank"]
+    row = (val.to_string(index=False))
     print(row)
-    print(df)
-    if datarow == row:
-        df.iloc[datarow, 2] += score
-    print(df)
+    df.loc[row, "chance_rank"] += score
     df.to_csv(r'database.csv', index=False)
 
 
-def get_response(message):
+def get_response(msg):
     df = pd.read_csv("database.csv")
-    df = pd.DataFrame(df, columns=['parent'])
-    getresponse = df.loc[df['parent'].str.match(message, case=False)]
-    for getresponse in df:
-        responses = {getresponse}
-        df2 = pd.DataFrame(responses)
-        print(df2)
+    try:
+        hold = (df[df['parent'] == msg].index[0])
+        filtered = df[df['parent'] == msg]
+        df = filtered[filtered['chance_rank'] == filtered['chance_rank'].max()]
+        dfresp = (df["child"])
+        return dfresp.to_string(index=False)
+    except:
+        print("Cannot find", msg)
+
 
 def record_message(message):
     pass
 
 
-if input().lower == "good":
-    print("Added to Database")
-    chancerequest = df("chance_rank") + 1
+def addchance(message):
+    if message.lower == "good":
+        print("Added to Database")
+        return df("chance_rank") + 1
 
-if input().lower == "bad":
-    print("Added to Database")
-    chancerequest = df("chance_rank") - 1
+    if message.lower == "bad":
+        print("Added to Database")
+        return df("chance_rank") - 1
 
 
 def explore():
@@ -74,24 +73,24 @@ def read_data():
     with open("database.csv") as f_obj:
         reader = csv.reader(f_obj, delimiter=',')
         for line in reader:
-            if input() in line:
+            if message in line:
                 found = found + 1
-                if input().lower not in ["good", "bad"]:
-                    if input() > found-1:
-                        print("String", f'"{input()}"', "found", f'{found} times')
+                if message.lower not in ["good", "bad"]:
+                    if message > found-1:
+                        print("String", f'"{message}"', "found", f'{found} times')
                     else:
-                        print("Error: Could not find term", input(), "Epsilon will be attempted")
+                        print("Error: Could not find term", message, "Epsilon will be attempted")
 
 
 while True:
-    message = input('>')
+    message = input('> ')
     record_message(message)
     response = get_response(message)
-    print(response)
-    score = input('Good? >')
-    if score == "good":
-        record_response_score(response, 1)
-    elif score == "bad":
-        record_response_score(response, -1)
+    print("AI:", response)
+    score = input('Good response? > ')
+    if score.lower() == "good":
+        record_response_score(message, 1, response)
+    elif score.lower() == "bad":
+        record_response_score(message, -1, response)
     else:
-        record_response_score(response, -0.1)
+        record_response_score(message, -0.1, response)
